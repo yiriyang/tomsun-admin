aaf2c40133c02342504c69e510c12207d77b26bb
fanhao <846186994@qq.com>
2023-10-15 21:40:01 +0800

    feat: set @ alies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,4 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+import '@rushstack/eslint-patch/modern-module-resolution'
 
 module.exports = {
   root: true,
--- vitest.config.ts
+++ vitest.config.ts
@@ -13,6 +13,11 @@ export default mergeConfig(
       transformMode: {
         web: [/\.[jt]sx$/]
       }
+    },
+    resolve: {
+      alias: {
+        '@': '/src'
+      }
     }
   })
 )
