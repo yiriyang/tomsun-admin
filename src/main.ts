981eaf7ed8cb280df4d5cea2f57031b19c327ba9
fanhao <846186994@qq.com>
2024-08-06 23:08:33 +0800

    feat: 安装unocss

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import "@/styles/main.css"
+import '@/styles/main.css'
+import 'virtual:uno.css'
 
-import { createApp } from "vue"
-import store from "@/stores"
+import { createApp } from 'vue'
+import store from '@/stores'
 
-import App from "./App.vue"
-import router from "./router"
+import App from './App.vue'
+import router from './router'
 
-import "@/router/permission"
+import '@/router/permission'
 
-import Antd from "ant-design-vue"
+import Antd from 'ant-design-vue'
 
 const app = createApp(App)
 
@@ -16,4 +17,4 @@ app.use(store)
 app.use(router)
 app.use(Antd)
 
-app.mount("#app")
+app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import unocss from "unocss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), unocss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   server: {
-    hmr: false
-  }
-})
+    hmr: false,
+  },
+});
